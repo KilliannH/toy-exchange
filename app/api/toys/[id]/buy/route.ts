feee3abe5cb183bbc51519aa2df899bee3bc931f
killiannh <killiannhervagault@gmail.com>
2025-08-31 23:36:44 +0200

    patch for vercel5

--- app/api/toys/[id]/buy/route.ts
+++ app/api/toys/[id]/buy/route.ts
@@ -4,13 +4,13 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { prisma } from "@/lib/prisma";
 
-export async function POST(req: Request, { params }: { params: { id: string } }) {
+export async function POST(req: Request, { params }: { params: Promise<{ id: string }> }) {
   const session = await getServerSession(authOptions);
   if (!session?.user) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
-
-  const toyId = params.id;
+  const paramsId = await params;
+  const toyId = paramsId.id;
   const buyerId = session.user.id;
 
   const toy = await prisma.toy.findUnique({
