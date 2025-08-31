9d4a47542ede93374f58010a0fc7827c0de5cf77
killiannh <killiannhervagault@gmail.com>
2025-09-01 00:34:37 +0200

    bugfix for builds

--- lib/withSignedUrls.ts
+++ lib/withSignedUrls.ts
@@ -1,15 +1,7 @@
-import { Storage } from "@google-cloud/storage";
-
-const storage = new Storage({
-  projectId: process.env.GCP_PROJECT_ID,
-  credentials: {
-    client_email: process.env.GCP_CLIENT_EMAIL,
-    private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  },
-});
-const bucket = storage.bucket(process.env.GCP_BUCKET_NAME!);
+import { getBucket } from "./storage";
 
 export async function withSignedUrls(toys: any[]) {
+    const bucket = getBucket();
   return Promise.all(
     toys.map(async (toy) => {
       const signedImages = await Promise.all(
